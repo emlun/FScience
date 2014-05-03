59c2e5dfa361f9510b1908f8bdfecf41e7928a5a
Emil Lundberg <lundberg.emil@gmail.com>
2014-05-03 21:34:00 +0200

    Remove unnecessary usings

--- FScience/FScienceTransfer.cs
+++ FScience/FScienceTransfer.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-using System.Text;
-using System.Net;
+
 namespace FScience {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class FScienceTransfer : MonoBehaviour {
--- FScience/Resources.cs
+++ FScience/Resources.cs
@@ -1,9 +1,5 @@
-using System;
-using KSP.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
+
 namespace FScience
 {
 	 public static class Resources
