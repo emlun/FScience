dc2e9fed007aa3d91e21b4167b3ddae7e8517306
Emil Lundberg <lundberg.emil@gmail.com>
2014-05-04 13:52:26 +0200

    Add Toolbar support

---
+++ FScienceToolbar/Resources.cs
@@ -0,0 +1,11 @@
+﻿namespace FScienceToolbar {
+    class Resources {
+        public const string toggleMainWindowButtonText = "FScience";
+        public const string toggleMainWindowButtonTooltip = "Toggle FScience transfer GUI";
+        public const string toggleMainWindowButtonTexturePath = "FScience/MainButton";
+
+        public const string transferExperimentsButtonText = "Transfer Experiments";
+        public const string transferExperimentsButtonTooltip = "Transfer all experiments to currently selected container";
+        public const string transferExperimentsButtonTexturePath = "FScience/TransferButton";
+    }
+}
